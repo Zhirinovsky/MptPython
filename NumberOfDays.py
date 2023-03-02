cfcb2257136eada561d99d2941adc499faaff61d
Ivan Ulesov <isip_i.p.ulesov@mpt.ru>
2023-03-02 10:40:20 +0300

    Изменил 2 задание

--- NumberOfDays.py
+++ NumberOfDays.py
@@ -3,14 +3,16 @@ min = 0; all = 0
 odd = {0, 2, 4, 6, 7, 9, 11}
 names = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
 
-for i in range(1, 29): min += i
+for i in range(1, 29): 
+
+    min += int(i/10) + int(i%10)
 
 for i in range(12):
     months[i] = min
     if i != 1: 
-        months[i] += 29 + 30
-        if i in odd: months[i] += 31
+        months[i] += 2 + 9 + 3
+        if i in odd: months[i] += 3 + 1
     print(f"{names[i]}: {months[i]} дней")
     all += months[i]
 
-print(f"Общее кол-во дней в году: {all} дней")
+print(f"Общее сумма дней в году: {all} дней")
